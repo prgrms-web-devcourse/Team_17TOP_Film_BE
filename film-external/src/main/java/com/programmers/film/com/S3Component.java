d193278d925cba16186f919a1a5eceffd07d7701
JEONMINKYU <16min99@gmail.com>
2021-12-01 17:57:03 +0900

    feat: s3 Setting the environment

---
+++ film-external/src/main/java/com/programmers/film/com/S3Component.java
@@ -0,0 +1,14 @@
+package com.programmers.film.com;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter @Setter
+@ConfigurationProperties(prefix = "cloud.aws.s3")
+@Component
+public class S3Component {
+
+    private String bucket;
+}
