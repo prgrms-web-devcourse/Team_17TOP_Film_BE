95dedfb344a0c1fcacf34d0f1b48fd37ce35823c
JEONMINKYU <16min99@gmail.com>
2021-12-02 18:47:55 +0900

    feat : aws s3 upload service

---
+++ film-external/src/main/java/com/programmers/film/img/S3Service.java
@@ -0,0 +1,66 @@
+package com.programmers.film.img;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.IOException;
+import java.util.UUID;
+import javax.annotation.PostConstruct;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@NoArgsConstructor
+public class S3Service {
+
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.credentials.accesskey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretkey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @PostConstruct
+    public void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        s3Client = AmazonS3ClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(this.region)
+            .build();
+    }
+
+    public String upload(MultipartFile file) throws IOException {
+        String fileName = createFileName(file.getOriginalFilename());
+
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+            .withCannedAcl(CannedAccessControlList.PublicRead));
+        return s3Client.getUrl(bucket, fileName).toString();
+
+    }
+
+    private String createFileName(String originalFileName) { //파일이름 유니크하게
+        return UUID.randomUUID().toString().concat(getFileExtension(originalFileName));
+    }
+
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(String.format("잘못된 형식의 파일 (%s) 입니다", fileName));
+        }
+    }
+}
