754982ac1263958d081fc1249c7e8e6b9cb3b5c2
jungeu1509 <jungeu1509@gmail.com>
2021-12-03 17:49:29 +0900

    feat : add repository

---
+++ film-domain/src/main/java/com/programmers/film/domain/post/repository/PostDetailRepository.java
@@ -0,0 +1,8 @@
+package com.programmers.film.domain.post.repository;
+
+import com.programmers.film.domain.post.domain.PostDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostDetailRepository extends JpaRepository<PostDetail, Long> {
+
+}
