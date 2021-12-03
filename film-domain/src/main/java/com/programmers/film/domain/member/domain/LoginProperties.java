e24533d8fc06257b6e497fa2ece7df79dfc11d0f
jungeu1509 <jungeu1509@gmail.com>
2021-12-03 16:35:09 +0900

    feat : add Entities draft
    - erase dummy entity
    - add entities draft

---
+++ film-domain/src/main/java/com/programmers/film/domain/member/domain/LoginProperties.java
@@ -0,0 +1,6 @@
+package com.programmers.film.domain.member.domain;
+
+public enum LoginProperties {
+    FILM,
+    KAKAO
+}
---
+++ film-domain/src/main/java/com/programmers/film/domain/post/domain/PostStatus.java
@@ -0,0 +1,5 @@
+package com.programmers.film.domain.post.domain;
+
+public enum PostStatus {
+    CLOSED, OPENABLE, OPENED
+}
