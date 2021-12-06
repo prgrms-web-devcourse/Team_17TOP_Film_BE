1b53c9dac5c327c5d1b3e50d74f22b13c874092e
iyj6707 <iyj6707@naver.com>
2021-12-07 03:02:01 +0900

    feat: Add Interceptor and Resolver
    To use @Auth, @UserId

---
+++ film-api/src/main/java/com/programmers/film/api/config/interceptor/AuthInterceptor.java
@@ -0,0 +1,32 @@
+package com.programmers.film.api.config.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final LoginCheckHandler loginCheckHandler;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        if (!(handler instanceof HandlerMethod handlerMethod)) {
+            return true;
+        }
+
+        Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
+        if (auth == null) {
+            return true;
+        }
+
+        Long userId = loginCheckHandler.getUserId(request);
+        request.setAttribute("user_id", userId);
+        return true;
+    }
+}
