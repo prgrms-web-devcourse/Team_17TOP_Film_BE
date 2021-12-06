77967091f3ac25157854643a3dc2172f23d7885a
iyj6707 <iyj6707@naver.com>
2021-12-07 02:59:40 +0900

    feat: add Exceptions

---
+++ film-common/src/main/java/com/programmers/film/common/error/exception/InternalServerErrorException.java
@@ -0,0 +1,12 @@
+package com.programmers.film.common.error.exception;
+
+public class InternalServerErrorException extends BusinessException {
+
+    public InternalServerErrorException(String message) {
+        super(message, ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    public InternalServerErrorException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
+    }
+}
