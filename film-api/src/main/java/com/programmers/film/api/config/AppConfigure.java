ba90df475a0b0842cb8986edbbd15035a75ff9b3
iyj6707 <iyj6707@naver.com>
2021-12-06 13:46:30 +0900

    refactor: remove unused property

--- film-api/src/main/java/com/programmers/film/api/config/AppConfigure.java
+++ film-api/src/main/java/com/programmers/film/api/config/AppConfigure.java
@@ -2,10 +2,7 @@ package com.programmers.film.api.config;
 
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -14,19 +11,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "app")
 public class AppConfigure {
 
-	private final Auth auth = new Auth();
 	private final OAuth2 oauth2 = new OAuth2();
-
-	@Getter
-	@Setter
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class Auth {
-
-		private String tokenSecret;
-		private long tokenExpiry;
-		private long refreshTokenExpiry;
-	}
 
 	public static final class OAuth2 {
 
