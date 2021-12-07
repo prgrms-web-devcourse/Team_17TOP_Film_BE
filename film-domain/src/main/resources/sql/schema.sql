3efe21f753a9aaf0e37ab2dae7e845de6ebdac42
iyj6707 <iyj6707@naver.com>
2021-12-07 17:13:47 +0900

    Fix: Fix 'user_id not found' error
    Add sqls to schema.sql

--- film-domain/src/main/resources/sql/schema.sql
+++ film-domain/src/main/resources/sql/schema.sql
@@ -2,6 +2,24 @@ DROP TABLE IF EXISTS auths CASCADE;
 DROP TABLE IF EXISTS group_permission CASCADE;
 DROP TABLE IF EXISTS groups CASCADE;
 DROP TABLE IF EXISTS permissions CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users
+(
+    `id`                      bigint       NOT NULL,
+    `nickname`                varchar(20)  NOT NULL,
+    `provider`                varchar(20)  NOT NULL,
+    `provider_id`             varchar(80)  NOT NULL,
+    `profile_image`           varchar(255) NULL,
+    `profile_thumbnail_image` varchar(255) NULL,
+    `created_at`              TIMESTAMP    NOT NULL,
+    `updated_at`              TIMESTAMP    NOT NULL,
+    `is_deleted`              tinyint(1)   NOT NULL,
+    `deleted_at`              TIMESTAMP    NULL,
+    `last_login_at`           TIMESTAMP    NULL,
+    CONSTRAINT PK_USERS PRIMARY KEY (id)
+);
+
 
 CREATE TABLE permissions
 (
@@ -36,10 +54,12 @@ CREATE TABLE auths
     provider_id   varchar(80) NOT NULL,
     profile_image varchar(255) DEFAULT NULL,
     group_id      bigint      NOT NULL,
+    user_id       bigint       DEFAULT NULL,
     PRIMARY KEY (id),
     CONSTRAINT unq_username UNIQUE (username),
     CONSTRAINT unq_provider_and_id UNIQUE (provider, provider_id),
-    CONSTRAINT fk_group_id_for_user FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+    CONSTRAINT fk_group_id_for_auths FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT fk_user_id_for_auths FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
 DROP TABLE IF EXISTS `users`;
