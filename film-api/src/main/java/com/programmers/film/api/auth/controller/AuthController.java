cdcb0c2391e0dc52a0a7fd6a911e69c5e98db3d1
iyj6707 <iyj6707@naver.com>
2021-12-07 15:34:08 +0900

    Feat: Add provider and providerId to Jwt

--- film-api/src/main/java/com/programmers/film/api/auth/controller/AuthController.java
+++ film-api/src/main/java/com/programmers/film/api/auth/controller/AuthController.java
@@ -1,12 +1,7 @@
 package com.programmers.film.api.auth.controller;
 
-import com.programmers.film.api.auth.dto.request.LoginRequest;
-import com.programmers.film.api.auth.dto.response.LoginResponse;
 import com.programmers.film.api.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
