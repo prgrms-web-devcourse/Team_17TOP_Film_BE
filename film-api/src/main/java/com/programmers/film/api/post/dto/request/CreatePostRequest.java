e2b938590125d4deeff6d19c17d85e00ef9d9abd
jungeu1509 <jungeu1509@gmail.com>
2021-12-07 18:01:10 +0900

    fix: fix Entity typo correction
    1. change name AuthorityRepository.java to PostAuthorityRepository.java
    2. fix PostAuthority member_id to user_id

--- film-api/src/main/java/com/programmers/film/api/post/dto/request/CreatePostRequest.java
+++ film-api/src/main/java/com/programmers/film/api/post/dto/request/CreatePostRequest.java
@@ -15,7 +15,7 @@ public class CreatePostRequest {
     private String previewText;
     private Point location;
     private String availableAt;
-    private List<String> authortities;
+    private List<String> authorities;
     private List<OrderImage> imageFiles;
     private String content;
     private String authorNickname;
--- film-domain/src/main/java/com/programmers/film/domain/post/repository/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.film.domain.post.repository;
-
-import com.programmers.film.domain.post.domain.PostAuthority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<PostAuthority, Long> {
-
-}
