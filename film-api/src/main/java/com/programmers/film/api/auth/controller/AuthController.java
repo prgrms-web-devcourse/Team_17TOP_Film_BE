19b9a59e52428674af8c77fe9a4b56d0317470c9
iyj6707 <iyj6707@naver.com>
2021-12-07 17:47:16 +0900

    feat: Add token expired check

--- film-api/src/main/java/com/programmers/film/api/auth/controller/AuthController.java
+++ film-api/src/main/java/com/programmers/film/api/auth/controller/AuthController.java
@@ -12,10 +12,63 @@ public class AuthController {
 
 	private final AuthService authService;
 
-//	@Auth
-//	@GetMapping(value = "/login")
-//	public JoinResponse join(JoinRequest joinRequest, @UserId Long userId) {
-//		return authService.join()
-//		return null;
+//	@GetMapping("/refresh")
+//	public void refreshToken (HttpServletRequest request, HttpServletResponse response) {
+//
+//		// check access token
+//		String accessToken = HeaderUtil.getAccessToken(request);
+//		Jwt.Claims claims = jwt.verify(accessToken);
+//		if (claims.checkExpired()) {
+//			// expired token
+//			return;
+//		}
+//
+//		String userId = claims.getSubject();
+//		RoleType roleType = RoleType.of(claims.get("role", String.class));
+//
+//		// refresh token
+//		String refreshToken = CookieUtil.getCookie(request, REFRESH_TOKEN_COOKIE_NAME)
+//			.map(Cookie::getValue)
+//			.orElse((null));
+//		AuthToken authRefreshToken = tokenProvider.convertAuthToken(refreshToken);
+//
+//		if (authRefreshToken.validate()) {
+//			return ApiResponse.invalidRefreshToken();
+//		}
+//
+//		// userId refresh token 으로 DB 확인
+//		UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUserIdAndRefreshToken(userId, refreshToken);
+//		if (userRefreshToken == null) {
+//			return ApiResponse.invalidRefreshToken();
+//		}
+//
+//		Date now = new Date();
+//		AuthToken newAccessToken = tokenProvider.createAuthToken(
+//			userId,
+//			roleType.getCode(),
+//			new Date(now.getTime() + appProperties.getAuth().getTokenExpiry())
+//		);
+//
+//		long validTime = authRefreshToken.getTokenClaims().getExpiration().getTime() - now.getTime();
+//
+//		// refresh 토큰 기간이 3일 이하로 남은 경우, refresh 토큰 갱신
+//		if (validTime <= THREE_DAYS_MSEC) {
+//			// refresh 토큰 설정
+//			long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpiry();
+//
+//			authRefreshToken = tokenProvider.createAuthToken(
+//				appProperties.getAuth().getTokenSecret(),
+//				new Date(now.getTime() + refreshTokenExpiry)
+//			);
+//
+//			// DB에 refresh 토큰 업데이트
+//			userRefreshToken.setRefreshToken(authRefreshToken.getToken());
+//
+//			int cookieMaxAge = (int) refreshTokenExpiry / 60;
+//			CookieUtil.clearCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
+//			CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, authRefreshToken.getToken(), cookieMaxAge);
+//		}
+//
+//		return ApiResponse.success("token", newAccessToken.getToken());
 //	}
 }
