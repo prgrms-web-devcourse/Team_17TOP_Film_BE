af2a50cbb0536643b0550af0db69d2b3c42172f5
jungeu1509 <jungeu1509@gmail.com>
2021-12-07 18:02:16 +0900

    fix: lombok

--- film-domain/src/main/java/com/programmers/film/domain/common/domain/BaseEntity.java
+++ film-domain/src/main/java/com/programmers/film/domain/common/domain/BaseEntity.java
@@ -4,11 +4,14 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Setter @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
