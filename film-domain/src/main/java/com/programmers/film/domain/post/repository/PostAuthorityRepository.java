b2f28637e8a4084e727082a7b9d0c7851ee8a4f9
JEONMINKYU <16min99@gmail.com>
2021-12-08 17:32:19 +0900

    fix : import remove, security test //

--- film-domain/src/main/java/com/programmers/film/domain/post/repository/PostAuthorityRepository.java
+++ film-domain/src/main/java/com/programmers/film/domain/post/repository/PostAuthorityRepository.java
@@ -2,7 +2,6 @@ package com.programmers.film.domain.post.repository;
 
 import com.programmers.film.domain.post.domain.PostAuthority;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.programmers.film.domain.member.domain.User;
 import java.util.Optional;
 
 public interface PostAuthorityRepository extends JpaRepository<PostAuthority, Long> {
