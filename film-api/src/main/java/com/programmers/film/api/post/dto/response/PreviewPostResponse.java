1e4913a5c29796036efa80fecaa19bae88ad6e10
JEONMINKYU <16min99@gmail.com>
2021-12-09 16:54:02 +0900

    hotfix: date type, add anotation

--- film-api/src/main/java/com/programmers/film/api/post/dto/response/PreviewPostResponse.java
+++ film-api/src/main/java/com/programmers/film/api/post/dto/response/PreviewPostResponse.java
@@ -1,5 +1,6 @@
 package com.programmers.film.api.post.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.programmers.film.api.post.dto.common.AuthorityImage;
 import com.programmers.film.api.post.dto.common.PointDto;
 import java.time.LocalDate;
@@ -10,10 +11,12 @@ import lombok.Getter;
 @Builder
 @Getter
 public class PreviewPostResponse {
+
     private Long postId;
     private String title;
     private String previewText;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate availableAt;
     private String state;
     private PointDto location;
