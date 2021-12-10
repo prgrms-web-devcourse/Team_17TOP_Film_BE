bbb7b9b60e5d86722a144ab361d1a7b26382c7db
Yeonjae Im <iyj6707@naver.com>
2021-12-10 15:35:16 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 BUILD_JAR=$(ls /home/ec2-user/app/deploy/film-api/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/app/deploy/deploy.log
+echo "> build file_name: $JAR_NAME" >> /home/ec2-user/app/deploy/deploy.log
 
-echo "> build 파일 복사" >> /home/ec2-user/app/deploy/deploy.log
+echo "> build copy file" >> /home/ec2-user/app/deploy/deploy.log
 DEPLOY_PATH=/home/ec2-user/app/deploy/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/app/deploy/deploy.log
+echo "> pid check" >> /home/ec2-user/app/deploy/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/app/deploy/deploy.log
+  echo "> There is no application currently running, so do not exit." >> /home/ec2-user/app/deploy/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
-  sleep 5
+  sleep 15
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/app/deploy/deploy.log
+echo "> DEPLOY_JAR deploy"    >> /home/ec2-user/app/deploy/deploy.log
 nohup java -jar $DEPLOY_JAR >> /home/ec2-user/app/deploy/deploy.log 2>/home/ec2-user/app/deploy/deploy_err.log &
