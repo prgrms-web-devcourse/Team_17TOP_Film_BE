b5d172aa3ceba8131b8cc9f82a1cff4b7a349351
jungeu1509 <jungeu1509@gmail.com>
2021-12-11 00:27:03 +0900

    fix: erase unused import

--- film-domain/src/main/java/com/programmers/film/domain/post/domain/PostDetail.java
+++ film-domain/src/main/java/com/programmers/film/domain/post/domain/PostDetail.java
@@ -4,7 +4,6 @@ import com.programmers.film.domain.user.domain.User;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
