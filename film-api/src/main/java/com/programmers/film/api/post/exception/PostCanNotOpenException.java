8ce3139684c8297abd9118f828d73ca2fd8de2d3
JEONMINKYU <16min99@gmail.com>
2021-12-11 22:55:21 +0900

    fix: Update getPostDetail error_code

--- film-api/src/main/java/com/programmers/film/api/post/exception/PostCanNotOpenException.java
+++ film-api/src/main/java/com/programmers/film/api/post/exception/PostCanNotOpenException.java
@@ -6,6 +6,6 @@ import com.programmers.film.common.error.exception.EntityNotFoundException;
 public class PostCanNotOpenException extends EntityNotFoundException {
 
     public PostCanNotOpenException(String message) {
-        super(message, ErrorCode.HANDLE_ACCESS_DENIED);
+        super(message, ErrorCode.CLOSED_POST_ERROR);
     }
 }
