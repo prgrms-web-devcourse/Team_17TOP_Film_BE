46c4a854d72c72466f882c59233d6b1ed40375f8
iyj6707 <iyj6707@naver.com>
2021-12-13 19:04:54 +0900

    test: test user controller

--- film-api/src/test/java/com/programmers/film/api/user/controller/UserControllerTest.java
+++ film-api/src/test/java/com/programmers/film/api/user/controller/UserControllerTest.java
@@ -2,29 +2,42 @@ package com.programmers.film.api.user.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.programmers.film.api.user.dto.request.SignUpRequest;
+import com.programmers.film.api.user.dto.response.CheckNicknameResponse;
+import com.programmers.film.api.user.dto.response.CheckUserResponse;
 import com.programmers.film.api.user.dto.response.UserResponse;
 import com.programmers.film.api.user.service.UserService;
-import com.programmers.film.common.error.GlobalExceptionHandler;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc(addFilters = false)
 @ContextConfiguration(classes = UserController.class)
 @WebMvcTest(UserController.class)
@@ -39,20 +52,9 @@ class UserControllerTest {
 	@MockBean
 	private UserService userService;
 
-	@Mock
-	private UserController userController;
-
-	@BeforeEach
-	public void setup() {
-		this.mockMvc = MockMvcBuilders.standaloneSetup(userController)
-			.setControllerAdvice(GlobalExceptionHandler.class)
-			.build();
-	}
-
 	@Test
 	@DisplayName("올바르지 못한 닉네임으로 회원가입하는 경우")
 	public void signupWithInvalidNicknameRequest() throws Exception {
-
 		// Given
 		final String nickname = "asdasd@d"; // Invalid nickname
 		final SignUpRequest signUpRequest = SignUpRequest.builder()
@@ -65,12 +67,14 @@ class UserControllerTest {
 		given(userService.signUp(any(), any())).willReturn(signUpResponse);
 
 		// When
-		final ResultActions resultActions = mockMvc.perform(post("/api/v1/users/signup")
+		final ResultActions resultActions = mockMvc.perform(
+			post("/api/v1/users/signup")
 				.content(objectMapper.writeValueAsString(signUpRequest))
-				.contentType(MediaType.APPLICATION_JSON))
-			.andDo(print());
+				.contentType(MediaType.APPLICATION_JSON)
+		);
 
 		// Then
-		resultActions.andExpect(status().isBadRequest());
+		resultActions.andExpect(status().isBadRequest())
+			.andDo(print());
 	}
 }
