ba92b9b52e6ef310336470f679cc278df0884fb2
iyj6707 <iyj6707@naver.com>
2021-12-14 18:31:29 +0900

    fix: add setting user_id logic in Auth when signup

--- film-domain/src/main/java/com/programmers/film/domain/user/domain/User.java
+++ film-domain/src/main/java/com/programmers/film/domain/user/domain/User.java
@@ -1,5 +1,8 @@
 package com.programmers.film.domain.user.domain;
 
+import static org.assertj.core.util.Preconditions.checkArgument;
+
+import com.programmers.film.domain.auth.domain.Auth;
 import com.programmers.film.domain.common.domain.BaseEntity;
 import com.programmers.film.domain.common.domain.ImageUrl;
 import com.programmers.film.domain.post.domain.Post;
@@ -7,6 +10,7 @@ import com.programmers.film.domain.post.domain.PostAuthority;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
@@ -17,6 +21,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
@@ -51,12 +56,6 @@ public class User extends BaseEntity {
     @Column(name = "provider_id")
     private String providerId;
 
-    @OneToMany(mappedBy = "user", orphanRemoval = true, fetch = FetchType.EAGER)
-    private List<PostAuthority> postAuthorities = new ArrayList<>();
-
-    @OneToMany(mappedBy = "author", orphanRemoval = true)
-    private List<Post> posts = new ArrayList<>();
-
     @Embedded
     @AttributeOverrides({
         @AttributeOverride(name = "originalSizeUrl", column = @Column(name = "profile_image")),
@@ -67,18 +66,25 @@ public class User extends BaseEntity {
     @Column(name = "last_login_at")
     private LocalDateTime lastLoginAt;
 
+    @OneToMany(mappedBy = "user", orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<PostAuthority> postAuthorities = new ArrayList<>();
+
+    @OneToMany(mappedBy = "author", orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
     @Builder
     public User(Long id, String nickname) {
-
         this.id = id;
         this.nickname = nickname;
     }
 
-    public void updateProvider(String provider, String providerId) {
+    public void setProvider(String provider, String providerId) {
+        checkArgument(provider != null, "provider must be provided.");
+        checkArgument(providerId != null, "providerId must be provided.");
+
         this.provider = provider;
         this.providerId = providerId;
     }
-
 
     public void addAuthority(PostAuthority authority) {
         postAuthorities.add(authority);
