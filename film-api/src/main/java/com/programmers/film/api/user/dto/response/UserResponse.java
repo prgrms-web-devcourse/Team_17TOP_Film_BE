d7415924e1dd5007c88c54d6f6e093f6066befc5
iyj6707 <iyj6707@naver.com>
2021-12-17 16:12:41 +0900

    test: test signup service

--- film-api/src/main/java/com/programmers/film/api/user/dto/response/UserResponse.java
+++ film-api/src/main/java/com/programmers/film/api/user/dto/response/UserResponse.java
@@ -2,6 +2,7 @@ package com.programmers.film.api.user.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @Builder
 @Getter
@@ -10,4 +11,12 @@ public class UserResponse {
 	private final String nickname;
 
 	private final String profileImageUrl;
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this)
+			.append("nickname", nickname)
+			.append("profileImageUrl", profileImageUrl)
+			.toString();
+	}
 }
