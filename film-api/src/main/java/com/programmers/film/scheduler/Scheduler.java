05a96ae5e70e3448b3945472b72de9020c480369
JEONMINKYU <16min99@gmail.com>
2021-12-17 16:21:30 +0900

    fix: scheduler

--- film-api/src/main/java/com/programmers/film/scheduler/Scheduler.java
+++ film-api/src/main/java/com/programmers/film/scheduler/Scheduler.java
@@ -5,7 +5,10 @@ import com.programmers.film.domain.post.domain.PostStatus;
 import com.programmers.film.domain.post.repository.PostRepository;
 import com.programmers.film.domain.post.repository.PostStateRepository;
 import java.time.LocalDate;
+
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,13 +19,15 @@ public class Scheduler {
 
     private final PostRepository postRepository;
     private final PostStateRepository postStateRepository;
+    private Logger log = LoggerFactory.getLogger(getClass());
 
-    @Scheduled(cron="01 00 00 * * ?", zone = "Asia/Seoul")
+    @Scheduled(cron="30 00 00 * * *", zone = "Asia/Seoul")
     @Transactional
     public void PostStateUpdateScheduled() {
         PostState postState = postStateRepository.findByPostStateValue(PostStatus.OPENABLE.toString()).get();
-
-        postRepository.findAllByAvailableAtBetween(LocalDate.now(),LocalDate.now())
+        LocalDate day = LocalDate.now();
+        log.info(day.toString());
+        postRepository.findAllByAvailableAtBetween(day,day)
             .forEach( post -> post.setState(postState));
     }
 }
